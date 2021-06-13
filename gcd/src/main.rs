4e2e6c36303449deb806cb8dd715eb15d5795831
Jim Blandy <jimb@red-bean.com>
2021-06-13 14:16:18 -0700

    gcd: Update for Rust 2018.

--- gcd/src/main.rs
+++ gcd/src/main.rs
@@ -1,3 +1,6 @@
+#![warn(rust_2018_idioms)]
+#![allow(elided_lifetimes_in_paths)]
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
@@ -20,19 +23,19 @@ fn test_gcd() {
                3 * 11);
 }
 
-use std::io::Write;
 use std::str::FromStr;
+use std::env;
 
 fn main() {
     let mut numbers = Vec::new();
 
-    for arg in std::env::args().skip(1) {
+    for arg in env::args().skip(1) {
         numbers.push(u64::from_str(&arg)
                      .expect("error parsing argument"));
     }
 
     if numbers.len() == 0 {
-        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        eprintln!("Usage: gcd NUMBER ...");
         std::process::exit(1);
     }
 
